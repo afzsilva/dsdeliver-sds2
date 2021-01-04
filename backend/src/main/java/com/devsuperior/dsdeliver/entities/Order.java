0a6e1833cdbac1779c96b656f0d42acfeba623b1
Afranio <afzsilva@gmail.com>
2021-01-04 19:17:11 -0300

    POST /orders

--- backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
@@ -41,6 +41,19 @@ public class Order implements Serializable {
 	}
 
 	
+	
+	public Order(Long id, String address, Double latitude, Double longitude, Instant moment, OrderStatus status) {
+		super();
+		this.id = id;
+		this.address = address;
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.moment = moment;
+		this.status = status;
+	}
+
+
+
 	public Long getId() {
 		return id;
 	}
