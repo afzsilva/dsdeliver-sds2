5ffb1e2e2c65a09e5b4a6dc7880b48e5c366d72e
Afranio <afzsilva@gmail.com>
2021-01-06 19:47:27 -0300

    Front end and Api Integrated Part 1

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,10 +1,28 @@
 import './styles.css';
 import React from 'react'
 import StepsHeader from './StepsHeader';
+import ProductsList from './ProductsList';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { Product } from "./types";
+import { fetchProducts } from "../api";
+
 export const Orders = () => {
+
+    const [products, setProducts] = useState<Product[]>([]);
+    console.log(products);
+
+    useEffect(() => {
+        fetchProducts()
+            .then(response => setProducts(response.data))
+            .catch(error => console.log(error));
+    }, []);
+
+
     return (
         <div className="orders-container">
             <StepsHeader />
+            <ProductsList products={products} />
         </div>
     )
 }
